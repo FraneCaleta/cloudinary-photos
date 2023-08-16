23384c46b0b861989d9febdc1b4fe42080e565d3
FraneCaleta <frane.caleta@yahoo.com>
2023-08-16 15:39:20 +0200

    Add gallery fetch images

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,3 +1,4 @@
+import { CloudinaryImage } from "./cloudinary-image";
 import UploadButton from "./upload-button";
 import cloudinary from "cloudinary";
 
@@ -9,16 +10,26 @@ export default async function GalleryPage() {
   const results = (await cloudinary.v2.search
     .expression("resource_type:image")
     .sort_by("public_id", "desc")
-    .max_results(5)
+    .max_results(30)
     .execute()) as { resources: SearchResult[] };
-
-  console.log(results);
 
   return (
     <section>
       <div className="flex justify-between">
         <h1 className="text-4xl font-bold">Gallery</h1>
         <UploadButton />
+
+        <div className="grid grid-cols-4 gap-4">
+          {results.resources.map((result) => (
+            <CloudinaryImage
+              key={result.public_id}
+              src={result.public_id}
+              alt="An image of something"
+              width="400"
+              height="300"
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
