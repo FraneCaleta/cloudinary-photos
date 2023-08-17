1c8e79acbce97410395d7dec9d4bbfa5663955cf
FraneCaleta <frane.caleta@yahoo.com>
2023-08-17 15:44:53 +0200

    CSS improvements of Gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -10,14 +10,16 @@ export default async function GalleryPage() {
   const results = (await cloudinary.v2.search
     .expression("resource_type:image")
     .sort_by("public_id", "desc")
-    .max_results(30)
+    .max_results(10)
     .execute()) as { resources: SearchResult[] };
 
   return (
     <section>
-      <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">Gallery</h1>
-        <UploadButton />
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Gallery</h1>
+          <UploadButton />
+        </div>
 
         <div className="grid grid-cols-4 gap-4">
           {results.resources.map((result) => (
