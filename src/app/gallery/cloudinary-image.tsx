589751e42c89e28fa6719cf623c7d6fec0d7040b
FraneCaleta <frane.caleta@yahoo.com>
2023-08-17 22:59:54 +0200

    Add heart on every image top corner

--- src/app/gallery/cloudinary-image.tsx
+++ src/app/gallery/cloudinary-image.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { Heart } from "@/components/icons/heart";
 import { CldImage } from "next-cloudinary";
 
 export function CloudinaryImage(props: any) {
-  return <CldImage {...props} />;
+  return (
+    <div className="relative">
+      <CldImage {...props} />
+      <Heart className="absolute top-2 right-2" />
+    </div>
+  );
 }
