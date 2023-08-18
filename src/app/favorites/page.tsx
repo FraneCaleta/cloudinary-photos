3010a9cd12b7ef6abbc1451e9f8019b66018ab18
FraneCaleta <frane.caleta@yahoo.com>
2023-08-18 17:24:35 +0200

    Favorites page implemented and links added

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,15 +1,10 @@
-import { CloudinaryImage } from "./cloudinary-image";
-import UploadButton from "./upload-button";
+import { CloudinaryImage } from "../gallery/cloudinary-image";
 import cloudinary from "cloudinary";
+import { SearchResult } from "../gallery/page";
 
-export type SearchResult = {
-  public_id: string;
-  tags: string[];
-};
-
-export default async function GalleryPage() {
+export default async function FavoritesPage() {
   const results = (await cloudinary.v2.search
-    .expression("resource_type:image")
+    .expression("resource_type:image AND tags=favorite")
     .sort_by("created_at", "desc")
     .with_field("tags")
     .max_results(30)
@@ -19,8 +14,7 @@ export default async function GalleryPage() {
     <section>
       <div className="flex flex-col gap-8">
         <div className="flex justify-between">
-          <h1 className="text-4xl font-bold">Gallery</h1>
-          <UploadButton />
+          <h1 className="text-4xl font-bold">Favorite Images</h1>
         </div>
 
         <div className="grid grid-cols-4 gap-4">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import { Heart } from "@/components/icons/heart";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,24 +23,27 @@ function SideMenu() {
           </h2>
           <div className="space-y-1">
             <Button
-              variant="secondary"
+              asChild
+              variant="ghost"
               className="w-full justify-start flex gap-2"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                />
-              </svg>
-              Gallery
+              <Link href="/gallery">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                  />
+                </svg>
+                Gallery
+              </Link>
             </Button>
             <Button variant="ghost" className="w-full justify-start flex gap-2">
               <svg
@@ -58,9 +62,15 @@ function SideMenu() {
               </svg>
               Albums
             </Button>
-            <Button variant="ghost" className="w-full justify-start flex gap-2">
-              <Heart />
-              Favorites
+            <Button
+              asChild
+              variant="ghost"
+              className="w-full justify-start flex gap-2"
+            >
+              <Link href="/favorites">
+                <Heart />
+                Favorites
+              </Link>
             </Button>
           </div>
         </div>
