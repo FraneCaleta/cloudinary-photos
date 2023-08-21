5789ec3a1c5da1ddc92f506e3cd25b2642fb71d6
FraneCaleta <frane.caleta@yahoo.com>
2023-08-21 08:19:55 +0200

    Gallery grid and search implementation

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,15 +1,22 @@
-import { CloudinaryImage } from "./cloudinary-image";
 import UploadButton from "./upload-button";
 import cloudinary from "cloudinary";
+import GalleryGrid from "./gallery-grid";
+import { SearchForm } from "./search-form";
 
 export type SearchResult = {
   public_id: string;
   tags: string[];
 };
 
-export default async function GalleryPage() {
+export default async function GalleryPage({
+  searchParams: { search },
+}: {
+  searchParams: {
+    search: string;
+  };
+}) {
   const results = (await cloudinary.v2.search
-    .expression("resource_type:image")
+    .expression(`resource_type:image${search ? ` AND tags=${search}` : ""}`)
     .sort_by("created_at", "desc")
     .with_field("tags")
     .max_results(30)
@@ -23,17 +30,9 @@ export default async function GalleryPage() {
           <UploadButton />
         </div>
 
-        <div className="grid grid-cols-4 gap-4">
-          {results.resources.map((result) => (
-            <CloudinaryImage
-              key={result.public_id}
-              imageData={result}
-              alt="An image of something"
-              width="400"
-              height="300"
-            />
-          ))}
-        </div>
+        <SearchForm initialSearch={search} />
+
+        <GalleryGrid images={results.resources} />
       </div>
     </section>
   );
